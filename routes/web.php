5bf8e6de02c827e22c30e637a88a6093e95f7495
Amirol Zolkifli <ikursus.com@gmail.com>
2017-09-12 15:05:34 +0800

    Update Controller

--- routes/web.php
+++ routes/web.php
@@ -1,65 +1,31 @@
 <?php
 
-Route::get('/', function () {
-
-    $message = 'Selamat Datang';
-    $field_nama = '<input type="text" name="nama_user">';
-
-    return view('welcome', compact('message', 'field_nama') );
-});
+Route::get('/', 'HomeController@index')->name('homepage');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboardUser');
+Route::get('/dashboard', 'HomeController@dashboard')->name('dashboardUser');
 
 Route::group(['prefix' => 'users'], function () {
 
-    Route::get('/', function () {
-        return view('users/senarai');
-    })->name('senaraiUsers');
-
-    Route::get('/add', function () {
-        return view('users/borang_add');
-    })->name('addUser');
-
-    Route::get('/{id}/edit', function () {
-        return view('users/borang_edit');
-    })->name('editUser');
+    Route::get('/', 'UsersController@index')->name('senaraiUsers');
+    Route::get('/add', 'UsersController@create')->name('addUser');
+    Route::get('/{id}/edit', 'UsersController@edit')->name('editUser');
 
 });
 
 Route::group(['prefix' => 'projects'], function () {
 
-    Route::get('/', function () {
-        return view('projects/senarai');
-    })->name('senaraiProjects');
-
-    Route::get('/add', function () {
-        return view('projects/borang_add');
-    })->name('addProject');
-
-    Route::get('/{id}/edit', function () {
-        return view('projects/borang_edit');
-    })->name('editProject');
+    Route::get('/', 'ProjectsController@index')->name('senaraiProjects');
+    Route::get('/add', 'ProjectsController@create')->name('addProject');
+    Route::get('/{id}/edit', 'ProjectsController@edit')->name('editProject');
 
 });
 
 Route::group(['prefix' => 'lokasi'], function () {
 
-    Route::get('/', function () {
-        return view('lokasi/senarai');
-    })->name('senaraiLokasi');
-
-    Route::get('/add', function () {
-        return view('lokasi/borang_add');
-    })->name('addLokasi');
-
-    Route::get('/{id}/edit', function () {
-        return view('lokasi/borang_edit');
-    })->name('editLokasi');
+    Route::get('/', 'LokasiController@index')->name('senaraiLokasi');
+    Route::get('/add', 'LokasiController@create')->name('addLokasi');
+    Route::get('/{id}/edit', 'LokasiController@edit')->name('editLokasi');
 
 });
